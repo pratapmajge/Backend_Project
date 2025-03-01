a2f7ab979cf59f012fe3792ac532297a0454e7ed
pratapmajge <majgepratap1504@gmail.com>
2025-03-02 00:32:02 +0530

    brcypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,4 +51,14 @@ const userSchema = new Schema({
 }
 )
 
+userSchema.pre("save" , async function(next) {
+    if(!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect =async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 export const User= mongoose.model("User" , userSchema)
